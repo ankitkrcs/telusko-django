a9339e50a7d4f3f8dfb9218da6ceee2b288db845
Ankit Maddheshiya <amddheshiya637@gmail.com>
2022-07-12 13:53:48 +0530

    initial commit

---
+++ calc/views.py
@@ -0,0 +1,17 @@
+from unicodedata import name
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"home.html",{'name':'Ankit'})
+
+
+
+def add(request):
+
+        val1 =int(request.POST["num1"])
+        val2 = int(request.POST["num2"])
+        res = val1 + val2
+        return render(request,"result.html",{"result":res})
+        
